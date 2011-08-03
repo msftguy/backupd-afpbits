4a5c764acfb8aeafd19c6b2a325f4ed641cc661a
msftguy <msftguy@MyBook.local>
2011-08-03 11:01:30 +0400

    Support non-AFP volumes too Added a mock structure for AFP fsctl Removed some redundant logging

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 //  Copyright 2011 msftguy. All rights reserved.
 //
 #include <stdio.h>
+#include <sys/errno.h>
 #include "../impHook/impHookApi.h"
 #include <mach-o/dyld.h>
 #include <mach-o/dyld_images.h>
@@ -15,26 +16,53 @@ typedef int	 (*pfn_fsctl_t)(const char *,unsigned long,void*,unsigned int);
 
 static pfn_fsctl_t s_origFsctl;
 
-int	 my_fsctl(const char * a1, unsigned long a2, void*a3, unsigned int a4);
+typedef struct {
+    unsigned int cmd1;
+    unsigned char unk0x04[0x4];
+    unsigned char flags1;
+    unsigned char unk0x9[7];
+    unsigned char unk0x10[0x8];
+    unsigned int cmd2;
+    unsigned int cmd3;
+    unsigned char unk0x20[0x10];
+} afp_fsctl_t;
 
-int	 my_fsctl(const char * a1, unsigned long a2, void*a3, unsigned int a4)
+// compile time assert
+#define ct_assert(e) extern char (*ct_assert(void)) [sizeof(char[1 - 2*!(e)])]
+ct_assert(sizeof(afp_fsctl_t) == 0x30);
+
+int	 my_fsctl(const char * a1, unsigned long a2, afp_fsctl_t *a3, unsigned int a4);
+
+int	 my_fsctl(const char * a1, unsigned long a2, afp_fsctl_t *io, unsigned int a4)
 {
     if (a2 == 0xC0307A1D) {
-        int result = fsctl(a1, a2, a3, a4); // Doesn't cause infinite recursion.. Maybe it should?.. Naah!
-        unsigned char* pOptByte = ((unsigned char*)a3) + 8;
-        if (result == 0 && ((*pOptByte & 3) != 3)) {
-            if (0 == (*pOptByte & 1)) {                
-                *pOptByte |= 1;
+        int cmd1 = io->cmd1;
+        int result = fsctl(a1, a2, io, a4); // Doesn't cause infinite recursion.. Maybe it should?.. Naah!
+        if (result != 0 && errno == 45) {// com.apple.backupd: afpfs fsctl failed to read settings: 45 Operation not supported
+            fprintf(stderr, "backupd_anyfs[%u]: Faking AFP fsctl support for a non-AFP volume .. cmd=[%u]\n", getpid(), cmd1);
+            fflush(stderr);
+            memset(io, 0, sizeof(afp_fsctl_t));
+            io->cmd1 = cmd1;
+            result = 0;
+        }
+        if (
+            (result == 0) && 
+            (cmd1 == 1) && // 1 means get flags, 2 means set .. I guess
+            ((io->flags1 & 3) != 3)
+            ) 
+        {
+            if (0 == (io->flags1 & 1)) {                
+                io->flags1 |= 1;
                 fprintf(stderr, "backupd_anyfs[%u]: Faking TM Lock Stealing support!\n", getpid());
                 fflush(stderr);
             }
-            if (0 == (*pOptByte & 2)) {                
-                *pOptByte |= 2;
+            if (0 == (io->flags1 & 2)) {                
+                io->flags1 |= 2;
                 fprintf(stderr, "backupd_anyfs[%u]: Faking Server Reply Cache support!\n", getpid());
                 fflush(stderr);
             }
         } else {
-            fprintf(stderr, "backupd_anyfs[%u]: result=%u, out[8]=%u\n", getpid(), result, ((char*)a3)[8]);
+            fprintf(stderr, "backupd_anyfs[%u]: result=%u, flags1=%u\n", getpid(), result, io->flags1);
             fflush(stderr);
         }
         return result;
@@ -42,7 +70,7 @@ int	 my_fsctl(const char * a1, unsigned long a2, void*a3, unsigned int a4)
 
     fprintf(stderr, "backupd_anyfs[%u]: my_fsctl(0x%lX) ignored\n", getpid(), a2);
     fflush(stderr);        
-    return s_origFsctl(a1, a2, a3, a4);
+    return fsctl(a1, a2, io, a4);
 }
 
 
@@ -70,16 +98,10 @@ void dl_main()
         fflush(stderr);
         return;
     }
-    pfn_fsctl_t origFsctl;
 
-    origFsctl = (pfn_fsctl_t)*pFsctlImp;
     const struct mach_header* procImageHeader = _dyld_get_image_header(0);
     _dyld_bind_fully_image_containing_address(procImageHeader);
     s_origFsctl = (pfn_fsctl_t)*pFsctlImp;
-    if (s_origFsctl != origFsctl) {
-        fprintf(stderr, "backupd_anyfs: A strange and mysterious thing has just happened.. %p->%p, just like that!\n", origFsctl, s_origFsctl);
-        fflush(stderr);
-    }
     *pFsctlImp = (uintptr_t)my_fsctl;
     fprintf(stderr, "backupd_anyfs: _fsctl hooked in %s (PID=%u)\n", _dyld_get_image_name(0), getpid());
     fflush(stderr);
